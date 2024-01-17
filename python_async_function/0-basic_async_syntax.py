be4c7b92864240cd36f2ebe52f9cb136848b1418
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2024-01-17 15:45:39 -0500

    correction type function and variables 0-basic_async_syntax.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,6 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     '''Waits for a random number of seconds.
     '''
-    random_value= random.uniform(0, max_delay)
+    random_value = random.uniform(0, max_delay)
     await asyncio.sleep(random_value)
     return random_value
