3b95787a182afc6b1a5db8dc0b7c16a0b3a80334
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2024-01-17 15:20:06 -0500

    correction type function and variables 0-basic_async_syntax.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 '''Task 0's module.
 '''
+import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    await asyncio.sleep(max_delay)
     return random.uniform(0, max_delay)
