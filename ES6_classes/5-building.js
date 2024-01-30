11f486ea8365ca81001e46f9f9d8cd2eaa642e44
Julibeto84 <juliangp670@gmail.com>
2024-01-30 15:01:45 -0500

    5-building.js

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+
+    constructor(sqft){
+        const estamosEnUnaClaseHija = new.target !== Building;
+        const noTieneElMethodoEvacuationWarningMessage = typeof this.evacuationWarningMessage === 'undefined';
+        if (estamosEnUnaClaseHija && noTieneElMethodoEvacuationWarningMessage) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+        
+        this.sqft = sqft
+    }
+
+    set sqft(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('sqft must be a number');
+        }
+
+        this._sqft = value;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+}
