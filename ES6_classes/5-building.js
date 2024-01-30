980f0f3a908010a615ec2bd13e54aa5dfe604c82
Julibeto84 <juliangp670@gmail.com>
2024-01-30 15:04:13 -0500

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,25 +1,25 @@
 export default class Building {
 
-    constructor(sqft){
-        const estamosEnUnaClaseHija = new.target !== Building;
-        const noTieneElMethodoEvacuationWarningMessage = typeof this.evacuationWarningMessage === 'undefined';
-        if (estamosEnUnaClaseHija && noTieneElMethodoEvacuationWarningMessage) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-        
-        this.sqft = sqft
+  constructor(sqft) {
+    const estamosEnUnaClaseHija = new.target !== Building;
+    const noTieneElMethodoEvacuationWarningMessage = typeof this.evacuationWarningMessage === 'undefined';
+    if (estamosEnUnaClaseHija && noTieneElMethodoEvacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
-    set sqft(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('sqft must be a number');
-        }
+    this.sqft = sqft
+  }
 
-        this._sqft = value;
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
 
-    get sqft() {
-        return this._sqft;
-    }
+    this._sqft = value;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 
 }
