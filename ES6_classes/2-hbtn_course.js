6849f3a86e1822da015f888c3f7000ce3a72c5fa
Julibeto84 <juliangp670@gmail.com>
2024-01-30 11:04:33 -0500

    update 2-hbtn_course

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,13 +2,7 @@
  * Represents a Holberton Course.
  */
 export default class HolbertonCourse {
-  /**
-   * Creates a new @see {@link HolbertonCourse}.
-   *
-   * @param {String} name - The name of the course.
-   * @param {Number} length - How long the course is (in months).
-   * @param {String[]} students - The names of students in the course.
-   */
+  
   constructor(name, length, students) {
     this.name = name;
     this.length = length;
