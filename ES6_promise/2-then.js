ad26b9a3fdf56c50fd349971cb4b000c603e6c2b
Julibeto84 <juliangp670@gmail.com>
2024-02-06 14:36:56 -0500

    2-

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,26 @@
+
+/**
+ * 
+ * @param {Promise} promise 
+ * @returns 
+ */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'succes'
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+
+
+
+
+
+
+
+
+
+
+
