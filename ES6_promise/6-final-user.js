32d372c411bbb2b9cd3459aef7fc0593d2c97db1
Julibeto84 <julibeto84@DESKTOP-D0GC1J4>
2024-02-07 18:56:33 -0500

    6-final-user.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,23 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((selected) =>
+      selected.map((item) => {
+        if (item.status === 'rejected') {
+          return {
+            status: item.status,
+            value: item.reason.toString(),
+          };
+        }
+        return {
+          status: item.status,
+          value: item.value,
+        };
+      })
+    );
+}
