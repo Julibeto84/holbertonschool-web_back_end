a375cfa558c6e3fc5af4cea117c86d060b88e622
Julibeto84 <juliangp670@gmail.com>
2024-02-07 15:13:54 -0500

    3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -7,5 +7,4 @@ export default function handleProfileSignup() {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
-
 }
