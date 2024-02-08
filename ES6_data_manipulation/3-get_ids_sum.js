676e4f7fbaee5808c6e7e6c3888ca4affdc8da03
Julibeto84 <juliangp670@gmail.com>
2024-02-08 12:54:34 -0500

    3-get_ids_sum.js

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,4 @@
 export default function getStudentIdsSum(students) {
-  const sum = students.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.id, 0);
+  const sum = students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
   return sum;
 }
