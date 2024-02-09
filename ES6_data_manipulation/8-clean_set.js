3177a17aa1af9aef7eb539a0d9ca9c0b5974893e
Julibeto84 <juliangp670@gmail.com>
2024-02-09 09:20:55 -0500

    8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-  if(set instanceof Set && typeof startString === 'string') {
+  if (set instanceof Set && typeof startString === 'string') {
     return Array.from(set)
-    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+      .filter((value) => typeof value === 'string' && value.startsWith(startString))
       .map((value) => value.slice(startString.length))
       .join('-');
   }
